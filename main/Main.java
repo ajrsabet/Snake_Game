1f2e68611e1cd4e1fb8ff5d0946305b992c7fdeb
John <jbhumph@gmail.com>
2025-01-24 08:18:42 -0800

    test

--- main/Main.java
+++ main/Main.java
@@ -1,9 +1,14 @@
 package main;
 
+// internal resources
+import ui.Menu;
+import util.TypeWriter;
+
 public class Main {
     public static void main(String[] args) {
         TypeWriter print = new TypeWriter();
         print.SlowType("\nWelcome to Snake! The best video game in the world!\n"); // Adjust the delay as needed
 
+        new Menu();
     }
 }
